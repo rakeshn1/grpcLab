ffe97d625328b0cf7b300f8dcc67b5c61a85e92b
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-10-25 17:13:56 -0700

    new test file added

---
+++ grpcDemo/src/main/java/Service/AsyncLoadBalancerTest2.java
@@ -0,0 +1,25 @@
+package Service;
+
+public class AsyncLoadBalancerTest2 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+//		for(int i=0;i<100;i++) {			
+//			ClientTrigger.addWork();
+//		}
+//		while(ClientTrigger.hasWork()) {
+//			Thread.sleep(1000);
+//		}
+		
+		AsyncGrpcClientLoadBalancer loadBalancer = new AsyncGrpcClientLoadBalancer();
+		
+		for(int i=0; i<100; i++) {			
+			loadBalancer.addWork();
+		}
+		
+		while(loadBalancer.hasWork()) {
+			Thread.sleep(1000);
+		}
+		
+	}
+}
